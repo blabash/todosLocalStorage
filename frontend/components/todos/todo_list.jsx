6c471989b32dc1cbb47dddefb5d976d985f9563e
brett <brett.labash@gmail.com>
2019-07-20 17:08:12 -0700

    Add Done/Undo functionality to individual todos

--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import TodoListItem from './todo_list_item'
 import TodoForm from './todo_form'
 
-function TodoList(props) {
-    const { todos, receiveTodo, removeTodo } = props
+function TodoList({ todos, receiveTodo, removeTodo }) {
     const todoItems = todos.map(todo => {
         return <TodoListItem 
                     key={todo.id} 
                     todo={todo}
-                    remove={removeTodo} />
+                    remove={removeTodo}
+                    receive={receiveTodo} />
     })
 
     return (
--- frontend/components/todos/todo_list_item.jsx
+++ frontend/components/todos/todo_list_item.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { receiveTodo } from '../../actions/todo_actions';
 
-function TodoListItem(props) {
-    const {todo, remove} = props
+function TodoListItem({ todo, remove, receive }) {
+    let toggleDone = (todo) => {
+        todo.done = !todo.done
+        receive(todo)
+    }
     return(
-        <li>{todo.title}: {todo.body}
+        <li>
+            {todo.title}: {todo.body}
+            <button onClick={() => toggleDone(todo)}>{todo.done === false ? 
+                'Done' : 'Undo'}</button>
             <button onClick={() => remove(todo)}>Delete</button>
         </li>
     )
